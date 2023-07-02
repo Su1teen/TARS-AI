a8ca446ef926ddfc3061e7650ee40fd4bc8df9d6
Su1teen <130832850+Su1teen@users.noreply.github.com>
2023-07-02 10:54:50 +0600

    Add files via upload

--- test.py
+++ test.py
@@ -80,7 +80,7 @@ import openai
 import streamlit as st
 from streamlit_chat import message
 
-openai.api_key = "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"
+openai.api_key = "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"
 def get_transcript(video_id):
     before, sep, after = video_id.partition('=')
     if len(after) > 0:
@@ -95,7 +95,9 @@ def get_transcript(video_id):
     return (whole.replace("\n", ""))
 
 def chat_completion(request, tutor_mode):
-    messages = []
+    messages = [
+        {"role": "system", "content": "You are now chatting with the TARS Assistant. Enjoy your conversation!"}
+    ]
 
     # Append past messages to the conversation
     if st.session_state['past']:
@@ -108,26 +110,28 @@ def chat_completion(request, tutor_mode):
     
     if "youtube.com" in request:
         transcript = get_transcript(request)
-        request = f"I am gonna give you transcript of video, you have to summarize it and create brief explanation of main points. Here is the text:{transcript}, After that ask me, whether I have questions and wait for my response. If only I understood everything, try to check my understanding of the video by suggesting to give me practice questions based on the video. After that ask me to give you answers and wait for my response, and then check them for correctness. If I don't understand the content of video try to ask what was difficult. Wait for my response and after that, explain them in more detail."
+        request = f"I am gonna give you transcript of video, you have to summarize it and create brief explanation of main points. Here is the text:{transcript}. After that ask me, whether I have questions and wait for my response. If I don't understand the content of video try to ask what was difficult. Wait for my response and after that, explain them in more detail."
         messages.append({"role": "assistant", "content": request})
         messages.append({"role": "user", "content": request})
+    elif "analogue" in request or "alternative" in request or "similar" in request or "correlat" in request:
+        request=f"Give me analogues by description I provide to the study resources. They must be at the same topic and idea, field, link is not neccesary. If I will tell you to give free ones, collect only free onces, you have permission to do so. The source is: {request}. Start response with the Phrase: There are analogues close by description or genre, they are:\n"
+        messages.append({"role": "assistant", "content": request})
+        messages.append({"role": "user", "content": request})    
+    elif "practice" in request or "test" in request or "exam" in request or "exercise" in request:
+        request=f"Give me about 5 or 10 exercies or questions related to topic I requested. If you summarized the transcript before you may also use information from that summary to create a question. You should, check my answers for correctness, and if I made a mistake then correct me. Now here is request: {request}. Start response with the Phrase: Let's start a test"
+        messages.append({"role": "assistant", "content": request})
+        messages.append({"role": "user", "content": request})    
     else:
         if tutor_mode:
             # Provide clues instead of direct answers
-            if "questions" in request.lower():
-                clue = "Ask me about specific topics or details from the video, and I will try to provide more information."
-                messages.append({"role": "assistant", "content": clue})
-            elif "answers" in request.lower():
-                clue = "Think about the information presented in the video and try to come up with your answers. Once you have them, let me know, and we can check them together."
-                messages.append({"role": "assistant", "content": clue})
-            elif "difficult" in request.lower():
-                clue = "Tell me which specific parts of the video you found challenging, and I will explain them in more detail."
-                messages.append({"role": "assistant", "content": clue})
-            else:
-                messages.append({"role": "user", "content": request})
+            request=f"Do not give me a direct answer, only give me some sort of advice or clue so that I can figure out the question myself. If I struggle, try to help me to figure out the problem and its solution. Now here is request: {request}"
+            messages.append({"role": "assistant", "content": request})
+            messages.append({"role": "user", "content": request})    
         else:
+            request=f"Just give me a direct answer to my response. Be friendly. Now here is request: {request}"
+            messages.append({"role": "assistant", "content": request})
             messages.append({"role": "user", "content": request})
-    
+            
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
